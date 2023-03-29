b3b53eacaa15ffc2fd0f7a9dbfb62515326e615d
Retover <lehasazonoff@mail.ru>
2023-03-29 15:53:00 +0300

    fix

--- src/Utils/searchCoords.js
+++ src/Utils/searchCoords.js
@@ -1,7 +1,7 @@
 export async function searchCoords(location, KEY) {
   try {
     let response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${location}
+      `https://api.openweathermap.org/geo/1.0/direct?q=${location}
       &limit=5&appid=${KEY}`
     );
     let result = await response.json();
